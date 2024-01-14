8f02ec7ba1ba8a87c88ffff59fa22ff4aa58cd79
Pavel Kirov <kirovpavel69@gmail.com>
2024-01-14 19:33:53 +0200

    preparations p.1: directories, assets, module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@src': './src',
+          '@redux': './src/redux',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@navigator': './src/navigator',
+          '@helpers': './src/helpers',
+          '@asyncStorage': './src/asyncStorage',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/assets/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { mainColors } from "./colors";
+
+// define own fontStyles used for the project
+export const fontFamily = 'Roboto-Regular';
+export const fontFamilyBold = 'Roboto-Medium';
+export const fontFamilyExtraBold = 'Roboto-Bold';
+
+const style = StyleSheet.create({
+  flex: {
+    flex: 1
+  },
+  fdRow: {
+    flexDirection: 'row'
+  },
+  w100: {
+    width: '100%'
+  }
+})
+
+const trackColorToggle = {
+  false: mainColors.white,
+  true: mainColors.black,
+};
+
+export {
+  style,
+  trackColorToggle
+}
